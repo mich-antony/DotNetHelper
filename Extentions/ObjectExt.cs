e3ac9f97b88fd0f357862dbb2ea40b250f2e25b9
mich_antony <mich_antony@yahoo.com>
2022-05-10 12:05:57 -0400

    Initial extension methods for objects

---
+++ Extentions/ObjectExt.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetHelper.Extentions
+{
+    public static partial class ObjectExt
+    {
+        public static bool IsNull<T> (this T obj)
+        {
+            return obj == null;
+        }
+
+        public static bool IsNotNull<T>(this T obj)
+        {
+            return obj != null;
+        }
+
+        public static T IfNullReturnOtherValORself<T>(this T value, T othervalue)
+        {
+            return value == null ? othervalue : value;
+        }
+    }
+}
